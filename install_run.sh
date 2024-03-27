2593fd250f3f30707174b216fbda7fafe9725139
Krishna Balaga <krbalaga@in.ibm.com>
2024-03-28 03:20:05 +0530

    updated deploy script

--- install_run.sh
+++ install_run.sh
@@ -1,31 +1,26 @@
 # Update and upgrade system packages
-echo "Updating and upgrading packages..."
+echo "====== Updating and upgrading packages ======"
 sudo apt update -y
 sudo apt upgrade -y
 
 # Install Ollama and its models
-echo "Installing Ollama..."
+echo "====== Installing Ollama ======"
 curl -fsSL https://ollama.com/install.sh | sh
 ollama pull llama2
 
 # Install python3-pip
-echo "Installing python3-pip..."
+echo "====== Installing python3-pip ======"
 sudo apt install python3-pip -y
 
 # Install Flask and Ollama Python packages
-echo "Installing Flask and Ollama Python packages..."
+echo "====== Installing Flask and Ollama Python packages ======"
 pip install flask ollama
 
 screen -S llaminator -m -d  python3 main.py
-echo "now running.."
-echo " __        __                          __                       __                         
-|  \      |  \                        |  \                     |  \                        
-| $$      | $$  ______   ______ ____   \$$ _______    ______  _| $$_     ______    ______  
-| $$      | $$ |      \ |      \    \ |  \|       \  |      \|   $$ \   /      \  /      \ 
-| $$      | $$  \$$$$$$\| $$$$$$\$$$$\| $$| $$$$$$$\  \$$$$$$\\$$$$$$  |  $$$$$$\|  $$$$$$\
-| $$      | $$ /      $$| $$ | $$ | $$| $$| $$  | $$ /      $$ | $$ __ | $$  | $$| $$   \$$
-| $$_____ | $$|  $$$$$$$| $$ | $$ | $$| $$| $$  | $$|  $$$$$$$ | $$|  \| $$__/ $$| $$      
-| $$     \| $$ \$$    $$| $$ | $$ | $$| $$| $$  | $$ \$$    $$  \$$  $$ \$$    $$| $$      
- \$$$$$$$$ \$$  \$$$$$$$ \$$  \$$  \$$ \$$ \$$   \$$  \$$$$$$$   \$$$$   \$$$$$$  \$$      
-                                                                                           
-                                                                                       "
+echo "====== now running ======> "
+echo " _     _                 _             _             
+| |   | |               (_)           | |            
+| |   | | __ _ _ __ ___  _ _ __   __ _| |_ ___  _ __ 
+| |   | |/ _` | '_ ` _ \| | '_ \ / _` | __/ _ \| '__|
+| |___| | (_| | | | | | | | | | | (_| | || (_) | |   
+\_____/_|\__,_|_| |_| |_|_|_| |_|\__,_|\__\___/|_| "
