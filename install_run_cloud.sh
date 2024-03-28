ef9e6e4358e01e683889c43f4a7e4d38a17807af
Krishna Balaga <krbalaga@in.ibm.com>
2024-03-28 18:44:53 +0530

    updated messaging

--- install_run_cloud.sh
+++ install_run_cloud.sh
@@ -34,4 +34,4 @@ source ~/.bashrc
 screen -S llaminator -m -d  python3 main.py
 echo -e "\n====== now running ======> \n"
 echo -e " _     _                 _             _             \n| |   | |               (_)           | |            \n| |   | | __ _ _ __ ___  _ _ __   __ _| |_ ___  _ __ \n| |   | |/ _\` | '_ \` _ \\| | '_ \\ / _\` | __/ _ \\| '__|\n| |___| | (_| | | | | | | | | | | (_| | || (_) | |   \n\\_____/_|\\__,_|_| |_| |_|_|_| |_|\\__,_|\\__\\___/|_|   \n                                                     \n                                                     "
-echo -e "\nYou can access the ui on http://<PublicIP> "
+echo -e "\nYou can access the ui on http://<PublicIP> \n"
