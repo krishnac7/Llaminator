dbb8f7b5fd6c7a506344433fd53e3fd40c0f560f
Krishna Balaga <krbalaga@in.ibm.com>
2024-04-01 01:38:59 +0530

    testing startup script

---
+++ temp.sh
@@ -0,0 +1,44 @@
+# Update and upgrade system packages
+echo -e "\n====== Updating and upgrading packages ======\n"
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install Ollama and its models
+echo -e "\n====== Installing required modules ======\n"
+curl -fsSL https://ollama.com/install.sh | sh
+echo "waiting for ollama to start..."
+sleep 10
+echo -e "\n====== Fetching llama2 7B ======\n"
+ollama pull llama2
+
+# Install python3-pip
+echo -e "\n====== Installing python3-pip ======\n"
+sudo apt install python3-pip -y
+
+# Install Flask and Ollama Python packages
+echo -e "\n====== Installing Flask and Python packages ======\n"
+pip install flask ollama
+
+#configure network
+echo -e "\n====== configuring firewall ====== \n"
+sudo ufw allow 80/tcp
+sudo ufw allow ssh
+yes | sudo ufw enable
+sudo ufw reload
+
+#setup ports
+echo -e "\n====== Setting up variables ====== \n"
+export PORT=80
+
+#setup startup scripts
+echo -e "\n====== Setting up startup scripts ====== \n"
+echo -e '#!/bin/bash\nexport PORT=80\nscreen -S llaminator -m -d  python3 /root/Llaminator/main.py' | sudo tee /etc/init.d/myScreenStartup.sh > /dev/null
+sudo chmod +x /etc/init.d/myScreenStartup.sh
+sudo update-rc.d myScreenStartup.sh defaults
+
+
+screen -S llaminator -m -d  python3 main.py
+echo -e "\n====== now running ======> \n"
+echo -e " _     _                 _             _             \n| |   | |               (_)           | |            \n| |   | | __ _ _ __ ___  _ _ __   __ _| |_ ___  _ __ \n| |   | |/ _\` | '_ \` _ \\| | '_ \\ / _\` | __/ _ \\| '__|\n| |___| | (_| | | | | | | | | | | (_| | || (_) | |   \n\\_____/_|\\__,_|_| |_| |_|_|_| |_|\\__,_|\\__\\___/|_|   \n                                                     \n                                                     "
+echo -e "\nYou can access the ui on http://<FloatingIP> \n"
+
