aa727db14166358e9df0165cb3760028af2a612c
Krishna Balaga <krbalaga@in.ibm.com>
2024-04-01 02:40:07 +0530

    added startup scripts

--- install_run_cloud.sh
+++ install_run_cloud.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 # Install Ollama and its models
-echo -e "\n====== Installing Ollama ======\n"
+echo -e "\n====== Installing pre-requisites ======\n"
 curl -fsSL https://ollama.com/install.sh | sh
 echo "waiting for ollama to start..."
 sleep 10
@@ -16,7 +16,7 @@ echo -e "\n====== Installing python3-pip ======\n"
 sudo apt install python3-pip -y
 
 # Install Flask and Ollama Python packages
-echo -e "\n====== Installing Flask and Ollama Python packages ======\n"
+echo -e "\n====== Installing Flask and Python packages ======\n"
 pip install flask ollama
 
 #configure network
@@ -30,7 +30,34 @@ sudo ufw reload
 echo -e "\n====== Setting up variables ====== \n"
 export PORT=80
 
-screen -S llaminator -m -d  python3 main.py
+#setup startup scripts
+echo -e "\n====== Setting up startup scripts ====== \n"
+#remove old service
+if systemctl --quiet is-active myScreenStartup.service; then
+    sudo systemctl stop myScreenStartup.service >/dev/null 2>&1
+    sudo systemctl disable myScreenStartup.service >/dev/null 2>&1
+    echo "Service stopped and disabled."
+else
+    echo "Service does not exist or is not running."
+fi
+if [ -f /etc/systemd/system/myScreenStartup.service ]; then
+    sudo rm /etc/systemd/system/myScreenStartup.service >/dev/null 2>&1
+    sudo systemctl daemon-reload >/dev/null 2>&1
+    sudo systemctl reset-failed >/dev/null 2>&1
+    echo "Service file removed and systemd reloaded."
+else
+    echo "Service file does not exist."
+fi
+
+#create new
+echo -e "[Unit]\nDescription=Start My Screen Session\nAfter=network.target\n\n[Service]\nType=forking\nEnvironment=\"PORT=80\"\nExecStart=/usr/bin/screen -dmS llaminator python3 /root/Llaminator/main.py\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/myScreenStartup.service > /dev/null
+sudo chmod +x /etc/init.d/myScreenStartup.sh
+sudo systemctl daemon-reload
+sudo systemctl enable myScreenStartup.service
+sudo systemctl start myScreenStartup.service
+
 echo -e "\n====== now running ======> \n"
 echo -e " _     _                 _             _             \n| |   | |               (_)           | |            \n| |   | | __ _ _ __ ___  _ _ __   __ _| |_ ___  _ __ \n| |   | |/ _\` | '_ \` _ \\| | '_ \\ / _\` | __/ _ \\| '__|\n| |___| | (_| | | | | | | | | | | (_| | || (_) | |   \n\\_____/_|\\__,_|_| |_| |_|_|_| |_|\\__,_|\\__\\___/|_|   \n                                                     \n                                                     "
 echo -e "\nYou can access the ui on http://<FloatingIP> \n"
+
+echo -e "\n**** If you are using a GPU Instance, you need to Reboot after first install ****\n"
